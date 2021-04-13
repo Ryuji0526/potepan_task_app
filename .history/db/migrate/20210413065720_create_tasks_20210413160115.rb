72cbe0cff592548d36f895f29b201c7d45da691a
渡邊瑠士 <ryuji@Ryuji-no-mba.local>
2021-04-13 16:19:04 +0900

    create model and controller

---
+++ .history/db/migrate/20210413065720_create_tasks_20210413160115.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title, presence: true, length: {maximum: 75}
+      t.date :start_date
+      t.date :end_date
+      t.boolean :all_day, acceptance: true, default
+
+      t.timestamps
+    end
+  end
+end
